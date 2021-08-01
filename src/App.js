275a8fa0dc80f3ca2716cfde4232c0bafddf7f03
Chris-Stevenson-Git <67566718+Chris-Stevenson-Git@users.noreply.github.com>
2021-08-01 18:23:25 +1000

    Updates to API request and deployed to Heroku

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import axios from 'axios'
 
 
 import Datagrid from './components/Datagrid'
 import Navigation from './components/Navigation'
 import Sidebar from './components/Sidebar'
+
+var url = "https://sob7yipykh.execute-api.ap-southeast-2.amazonaws.com/data-insights-sample-data";
+
+var xhr = new XMLHttpRequest();
+
+const api_url = 'https://sob7yipykh.execute-api.ap-southeast-2.amazonaws.com/data-insights-sample-data';
+// const api_url = './data-insights-sample-data.json';
+// const api_url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 class App extends React.Component {
 
@@ -14,27 +23,24 @@ class App extends React.Component {
     data: []
   }
 
+
   componentDidMount(){
-    // fetch('https://sob7yipykh.execute-api.ap-southeast-2.amazonaws.com/data-insights-sample-data/',{ 
-    // method: 'GET',
-    // mode: 'no-cors',
-    //   headers: {
-    //     "Access-Control-Allow-Origin": "*",
-    //     'Access-Control-Allow-Methods' :    'GET',
-    //     "Content-type": "application/json",
-    //   }
-    // })
-    // .then(res => {
-    //   console.log(res)
-    //   // res.json()
-    // })
-    // .then((result) => {
-    //   console.log('Loaded');
-    //   this.setState({
-    //     isLoaded: true,
-    //     data: result.data
-    //   });
-    // })
+
+    fetch('https://sob7yipykh.execute-api.ap-southeast-2.amazonaws.com/data-insights-sample-data')
+        .then(async response => {
+            const data = await response.json();
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response statusText
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            this.setState({ data: data })
+        })
+        .catch(error => {
+            this.setState({ errorMessage: error.toString() });
+            console.error('There was an error!', error);
+        });
 
   
   }

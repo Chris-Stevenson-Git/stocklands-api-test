3c742af05c1517376ce725eafeb42b49564afda3
Chris-Stevenson-Git <67566718+Chris-Stevenson-Git@users.noreply.github.com>
2021-08-01 19:25:36 +1000

    Testing fetch request changes on heroku

--- src/App.js
+++ src/App.js
@@ -20,27 +20,36 @@ class App extends React.Component {
 
   state = {
     isLoaded: false,
-    data: []
+    // data: [{"Business Unit": "Retail", "Post Origin": "Inbound", "Published Date": "6/05/2021 0:00", "Post Type": "Replies", "Region": "NSWS", "Sentiment": "Neutral", "Classification Type": "Shopping Centre", "Case Type": "Referral", "Sub Type": NaN, "indicator": NaN, "Topic": NaN}, {"Business Unit": "Residential", "Post Origin": "Inbound", "Published Date": "1/05/2021 0:00", "Post Type": "Unpublished Posts", "Region": "QLD", "Sentiment": "Neutral", "Classification Type": NaN, "Case Type": "Referral", "Sub Type": "Not provided", "indicator": NaN, "Topic": "No Content"}, {"Business Unit": "Retail", "Post Origin": "Inbound", "Published Date": "6/05/2021 0:00", "Post Type": "Replies", "Region": "NSWS", "Sentiment": "Neutral", "Classification Type": NaN, "Case Type": NaN, "Sub Type": NaN, "indicator": NaN, "Topic": NaN}, {"Business Unit": "Retail", "Post Origin": "Inbound", "Published Date": "2/03/2021 0:00", "Post Type": "Comments", "Region": "NSWS", "Sentiment": "Neutral", "Classification Type": "Shopping Centre", "Case Type": "Comment", "Sub Type": NaN, "indicator": NaN, "Topic": NaN}, {"Business Unit": "Residential", "Post Origin": "Inbound", "Published Date": "13/05/2021 0:00", "Post Type": "Comments", "Region": "NSW", "Sentiment": "Neutral", "Classification Type": NaN, "Case Type": "Referral", "Sub Type": "Not provided", "indicator": NaN, "Topic": "No Content"}, {"Business Unit": "Retail", "Post Origin": "Inbound", "Published Date": "25/04/2021 0:00", "Post Type": "Comments", "Region": "QLD", "Sentiment": "Positive", "Classification Type": "Shopping Centre", "Case Type": "Feedback", "Sub Type": "Events & Entertainment", "indicator": NaN, "Topic": "Event - Kids Activities"}]
   }
 
 
   componentDidMount(){
 
+    // fetch('https://sob7yipykh.execute-api.ap-southeast-2.amazonaws.com/data-insights-sample-data')
+    //     .then(async response => {
+    //         const data = await JSON.parse(response.replace(/\bNaN\b/g, "null"));
+    //         // check for error response
+    //         if (!response.ok) {
+    //             // get error message from body or default to response statusText
+    //             const error = (data && data.message) || response.statusText;
+    //             return Promise.reject(error);
+    //         }
+    //         this.setState({ data: data })
+    //     })
+    //     .catch(error => {
+    //         this.setState({ errorMessage: error.toString() });
+    //         console.error('There was an error!', error);
+    //     });
+
     fetch('https://sob7yipykh.execute-api.ap-southeast-2.amazonaws.com/data-insights-sample-data')
-        .then(async response => {
-            const data = await JSON.parse(response.replace(/\bNaN\b/g, "null"));
-            // check for error response
-            if (!response.ok) {
-                // get error message from body or default to response statusText
-                const error = (data && data.message) || response.statusText;
-                return Promise.reject(error);
-            }
-            this.setState({ data: data })
-        })
-        .catch(error => {
-            this.setState({ errorMessage: error.toString() });
-            console.error('There was an error!', error);
-        });
+    .then(response => {
+      console.log('received response');
+      response.json();
+    })
+    .then(data => {
+      console.log(data)
+    })
 
   
   }
